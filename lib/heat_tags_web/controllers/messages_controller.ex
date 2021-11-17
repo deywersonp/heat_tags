4596e2d76f9bcdd75845f0ee4f262f11fe6a0935
Deywerson Pereira <deywerson.pereira@gmail.com>
2021-11-17 18:52:34 -0300

    Created first route (messages)

---
+++ lib/heat_tags_web/controllers/messages_controller.ex
@@ -0,0 +1,8 @@
+defmodule HeatTagsWeb.MessagesController do
+  use HeatTagsWeb, :controller
+
+  def create(conn, params) do
+    IO.inspect(params)
+    text(conn, "RECEBI A REQUISIÇÃO")
+  end
+end
